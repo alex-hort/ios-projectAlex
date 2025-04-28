b12a5c43d447cfa125c8eaffd0c0570ab55bef83
“alex-hort” <ahortealese001@alumno.uaemex.mx>
2025-04-28 16:04:56 -0600

    add screen to each pages

--- Bankey/Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -12,14 +12,34 @@ class OnboardingViewController: UIViewController{
     let imageView = UIImageView()
     let label = UILabel()
     
+    let heroImageName: String
+    let titleText: String
+    
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad(){
         super.viewDidLoad()
+        style()
+        layout()
     }
+    
+  
+    
+   
 }
 
 
 extension OnboardingViewController{
     func style(){
+        view.backgroundColor = .systemBackground
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 20
@@ -27,7 +47,7 @@ extension OnboardingViewController{
         //Image
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         //label
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -35,7 +55,7 @@ extension OnboardingViewController{
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
-        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that that will make you fill like you are back in 1989"
+        label.text =  titleText
     }
     
     func layout(){
