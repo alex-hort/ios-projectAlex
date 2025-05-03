875e7b4870c73bdeee395b49deb0ddee0da56f6a
“alex-hort” <ahortealese001@alumno.uaemex.mx>
2025-05-03 13:34:30 -0600

    UIViewC updatep

--- Bankey/Bankey/Utils/UIViewController+Utils.swift
+++ Bankey/Bankey/Utils/UIViewController+Utils.swift
@@ -6,16 +6,15 @@
 //
 import UIKit
 
-extension UIViewController{
-    func setStatusBar(){
-        let statusBarSize = UIApplication.shared.statusBarFrame.size
-        let frame = CGRect(origin: .zero, size: statusBarSize)
-        let statusbarView = UIView(frame: frame)
-        
-        statusbarView.backgroundColor = appColor
-        view.addSubview(statusbarView)
-        
+extension UIViewController {
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
     }
+
     
     func setTabBarImage(imageName: String, title: String){
         let configuration = UIImage.SymbolConfiguration(scale: .large)
