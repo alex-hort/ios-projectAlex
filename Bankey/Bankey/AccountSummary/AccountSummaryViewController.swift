911642bd5cc9dde3a09a21c5f5e0a18c234a31e2
“alex-hort” <ahortealese001@alumno.uaemex.mx>
2025-05-26 14:27:51 -0600

    feat: Group account summary newtork calls together

--- Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -29,19 +29,16 @@ class AccountSummaryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-        setupNavigationBar()
     }
-    func setupNavigationBar(){
-        navigationItem.rightBarButtonItem = logoutBarButtonItem
-    }
+ 
 }
 
 extension AccountSummaryViewController {
     private func setup() {
+        setupNavigationBar()
         setupTableView()
         setupTableHeaderView()
-//        fetchAccounts()
-        fetchDataAndLoadViews()
+        fetchData()
     }
     
     private func setupTableView() {
@@ -64,14 +61,18 @@ extension AccountSummaryViewController {
         ])
     }
     private func setupTableHeaderView(){
-    
+        
         
         var size = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
         size.width = UIScreen.main.bounds.width
         headerView.frame.size = size
         tableView.tableHeaderView = headerView
     }
+    func setupNavigationBar(){
+        navigationItem.rightBarButtonItem = logoutBarButtonItem
+    }
 }
+
 
 extension AccountSummaryViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -105,30 +106,39 @@ extension AccountSummaryViewController{
 
 // MARK: - Networking
 extension AccountSummaryViewController {
-    private func fetchDataAndLoadViews() {
+    private func fetchData() {
+        let group = DispatchGroup()
         
+        group.enter()
         fetchProfile(forUserId: "1") { result in
             switch result {
             case .success(let profile):
                 self.profile = profile
                 self.configureTableHeaderView(with: profile)
-                self.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            group.leave()
         }
 
+        group.enter()
         fetchAccounts(forUserId: "1") { result in
-                   switch result {
-                   case .success(let accounts):
-                       self.accounts = accounts
-                       self.configureTableCells(with: accounts)
-                       self.tableView.reloadData()
-                   case .failure(let error):
-                       print(error.localizedDescription)
-                   }
-               }
-           }
+            switch result {
+            case .success(let accounts):
+                self.accounts = accounts
+                self.configureTableCells(with: accounts)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+            group.leave()
+        }
+        
+        //Only be run if the  fetchProfile and fetchAccounts are completed
+        group.notify(queue: .main){
+            self.tableView.reloadData()
+        }
+        
+    }
     
     
     private func configureTableHeaderView(with profile: Profile) {
